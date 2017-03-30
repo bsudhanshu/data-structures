75b600d48b6331ecd1984b076b7284873f0f9960
Hack Reactor Student <student@hackreactor.com>
2017-03-30 11:19:03 -0700

    Pass tests for prototypal pattern (queue)

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,31 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = Object.create(queueMethods);
+  newQueue.enqueueCounter = 0;
+  newQueue.dequeueCounter = 0;
+
+  return newQueue;
 };
 
 var queueMethods = {};
+
+queueMethods.enqueue = function(value) {
+  this[this.enqueueCounter] = value;
+  this.enqueueCounter++;
+};
+
+queueMethods.dequeue = function() {
+  this.dequeueCounter++;
+  return this[this.dequeueCounter-1];      	
+};
+
+queueMethods.size = function() {
+  if (this.enqueueCounter - this.dequeueCounter < 0) {
+    return 0;
+  }	else {
+    return this.enqueueCounter - this.dequeueCounter;	
+  }
+};
 
 
