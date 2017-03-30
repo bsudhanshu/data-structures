f6bbedc7d5bfb8feab10b6482c6e49ea585e61c8
Hack Reactor Student <student@hackreactor.com>
2017-03-30 15:38:39 -0700

    Pass tests for pseudoclassical pattern (queue)

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -5,17 +5,17 @@ var Queue = function() {
   this.dequeueCounter = 0;
 };
 
-Queue.prototype.enqueue = function(value){
+Queue.prototype.enqueue = function(value) {
   this[this.enqueueCounter] = value;
   this.enqueueCounter++;
 };
 
-Queue.prototype.dequeue = function(){
+Queue.prototype.dequeue = function() {
   this.dequeueCounter++;
   return this[this.dequeueCounter - 1];
 };
 
-Queue.prototype.size = function(value){
+Queue.prototype.size = function(value) {
   if(this.enqueueCounter - this.dequeueCounter < 0) {
     return 0;
   } else {
